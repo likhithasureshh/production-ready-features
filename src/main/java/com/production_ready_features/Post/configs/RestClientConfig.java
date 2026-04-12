54bf7b625fca2b004e721a22a69e692d115a3be7
Likitha Suresh <likithasuresh@Likithas-MacBook-Air.local>
2026-04-12 14:29:43 +0530

    modified the bean of restclient

--- src/main/java/com/production_ready_features/Post/configs/RestClientConfig.java
+++ src/main/java/com/production_ready_features/Post/configs/RestClientConfig.java
@@ -7,6 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClient;
 
+
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @Configuration
 public class RestClientConfig {
     @Value("${employee.base.url}")
@@ -16,6 +21,7 @@ public class RestClientConfig {
     public RestClient getRestClientForEmployee() {
         return RestClient.builder()
                 .baseUrl(BASE_URL)
+                .defaultHeader(CONTENT_TYPE,APPLICATION_JSON_VALUE)
                 .defaultStatusHandler(HttpStatusCode::is5xxServerError,(req,res)->
                 {
                     System.out.println(new String(res.getBody().readAllBytes()));
